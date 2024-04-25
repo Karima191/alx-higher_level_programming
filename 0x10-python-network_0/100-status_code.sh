1d5e470b389ea700c701378987ee1c2b750405cb
Karima191 <karima.sel91@gmail.com>
2024-04-25 21:24:06 +0000

    0x10-python-network_0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
+# Script that sends a request to a URL passed as an argument, and displays only the status code of the response
+curl -so /dev/null -w "%{http_code}" "$1"
